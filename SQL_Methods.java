fc3fa473aae1beaf3b0cf21ccfb157d32d7a6531
Shinishki <bachirboutaleb@gmail.com>
2023-12-18 21:17:04 +0100

    Create SQL_Methods.java

---
+++ SQL_Methods.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.sql.*;
+public class sql_test_actually_its_easier_than_I_thought {
+    String getSQL(String str1, int columnIndex) {
+        String query = "select * from login_table where username=?";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_accounts", "root", "");
+            PreparedStatement ps;
+            ResultSet rs = null;
+            ps = con.prepareStatement(query);
+
+            ps.setString(1, str1);
+
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "Login successfully");
+                System.out.println(rs.next());
+
+                return rs.getString(columnIndex);
+            } else {
+                JOptionPane.showMessageDialog(null, "Erorr");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return "Error";
+
+    }
+
+    int change_username(String str1, String str2) {
+        String query = "update login_table set username=? where username=?";
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_accounts", "root", "");
+            PreparedStatement ps;
+            ResultSet rs = null;
+            ps = con.prepareStatement(query);
+            ps.setString(1, str2);
+            ps.setString(2, str1);
+            ps.executeUpdate();
+            return 1;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    int change_password(String str1, String str2) {
+        String query = "update login_table set password=? where password=?";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_accounts", "root", "");
+            PreparedStatement ps;
+            ResultSet rs = null;
+            ps = con.prepareStatement(query);
+            ps.setString(1, str2);
+            ps.setString(2, str1);
+            ps.executeUpdate();
+            return 1;
+        } catch (SQLException|ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    void delete_account(String str1){
+        String query = "delete from login_table where username=?";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_accounts", "root", "");
+            PreparedStatement ps;
+            ResultSet rs = null;
+            ps = con.prepareStatement(query);
+            ps.setString(1, str1);
+            ps.executeUpdate();
+        } catch (SQLException |ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
